917a719beabd0c32e5e33060f6ea20ed39052bfe
Ray-Mun-Ben <raymond.mungai@strathmore.edu>
2025-02-18 20:29:17 +0300

    More changes to the paths, now on the landing page to login/register

--- views/auth/forgot_password.php
+++ views/auth/forgot_password.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-require_once 'database.php';
-require_once 'user.php';
+require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../../models/User.php';
+
 require_once 'mailer.php';
 
 $error = "";
--- views/auth/reset_password.php
+++ views/auth/reset_password.php
@@ -1,6 +1,7 @@
 <?php
-require_once 'database.php';
-require_once 'user.php';
+require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../../models/User.php';
+
 
 session_start();
 
